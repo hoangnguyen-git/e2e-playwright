951544f53c4cf0bd960897ccbbd7d7f267303384
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro-3.local>
2023-08-07 15:36:24 +0700

    fix tear down file

--- configs/global-teardown.ts
+++ configs/global-teardown.ts
@@ -1,11 +1,5 @@
-import path from 'path';
-import AdmZip from 'adm-zip';
-
 async function globalTeardown() {
-    const reportPath = path.join(__dirname, `../html-report`);
-    const zip = new AdmZip();
-    zip.addLocalFolder(reportPath, `../html-report`);
-    zip.writeZip(`../html-report.zip`);
+    console.log("Teardown steps here")
 }
 
 export default globalTeardown;
